5324521dcd734e1042963ee17789a1f465f60742
Samuel <samnmbuguah@gmail.com>
2023-04-15 14:30:12 +0300

    first commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EmojiButton from './EmojiButton';
+function Header() {
+    return (
+      <header>
+        <h1>PetLand</h1>
+        <nav>
+          <EmojiButton emoji="👤" label="Login" />
+        </nav>
+      </header>
+    )
+  }
+  export default Header
